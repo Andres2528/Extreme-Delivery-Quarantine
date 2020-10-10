d1eaf16863c9ef857a4f0b65b18ce181118dfc22
Andrés López <56266820+Andreslp08@users.noreply.github.com>
2020-10-09 20:08:51 -0500

    Nueva versión
    uso de Jquery para agregar navbar y footer, navbar y footer responsivo.
    continuación del home.

---
+++ pageScripts/AddElements.js
@@ -0,0 +1,16 @@
+
+// AGREGAR NAVBAR
+function addNavBar(){
+    $(document).ready(function () {
+        $('#navBar').load('./navbar.html');
+      });
+}
+addNavBar();
+
+// AGREGAR FOOTER
+function addFooter(){
+    $(document).ready(function () {
+        $('#footer').load('./footer.html');
+      });
+}
+addFooter();
